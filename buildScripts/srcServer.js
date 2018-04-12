b427b0d41384cf69e544eb0374e5f60aa5064433
abhi-jha <abhi_jha1995@rediffmail.com>
2018-04-12 21:24:42 +0530

    babel transpilation added for ES6 support

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,12 @@
-var express = require('express');
+import express from 'express';
 
-var path = require('path');
+import path from 'path';
 
-var open = require('open');
+import open from 'open';
 
-var port = 3000;
+const port = 3000;
 
-var app = express();
+const app = express();
 
 app.get('/', function(req, res){
   res.sendfile(path.join(__dirname,'../src/index.html'));
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,5 @@
+//varSS chalk = require('chalk');
+
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode...'));
